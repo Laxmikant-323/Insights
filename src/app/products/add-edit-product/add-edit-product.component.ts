735bb1014dd4f56ef6a873254d82ada929487b5c
laxmikant <laxmikant.pyarelal1010@gmail.com>
2024-12-30 12:33:10 +0530

    Initial commit

---
+++ src/app/products/add-edit-product/add-edit-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pm-add-edit-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-edit-product.component.html',
+  styleUrls: ['./add-edit-product.component.css']
+})
+export class AddEditProductComponent {
+
+}
